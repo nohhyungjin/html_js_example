af5d6d2d7e0ead0e0d925daa393530a6c107a4fd
nohhyungjin <202130113@email.daelim.ac.kr>
2022-12-08 16:27:01 +0900

    정규식으로 체크

--- js/main.js
+++ js/main.js
@@ -39,17 +39,51 @@ nameH1Element.onclick = () => {
     inputModalElement.showModal();
 };
 
+const IsValidateName = (str) => {
+    return /^[a-zA-Zㄱ-힣0-9-_.]{2,12}$/.test(str)
+  }
+
+const IsValidateNum = (str) => {
+    return /^[0-9]*$/.test(str)
+}
+
+const IsValidateEmail = (email) => {
+    return /^[A-Za-z0-9.\-_]+@([A-Za-z0-9-]+\.)+[A-Za-z]{2,6}$/.test(email)
+}
+  
+  
+
 const modalSubmitBtn = document.querySelector('button.modalSubmit');
 
 modalSubmitBtn.onclick = () => {
     const modalFormElement = document.querySelector('.modalForm');
     const formData = new FormData(modalFormElement);
 
+    
+
     for (const [key, value] of formData) {
         localStorage.setItem(key, value);
-        if (key === 'userName') setUserName(value);
-        if (key === 'studentNo') setUserNumber(value);
-        if (key === 'email') setUserEmail(value);
+        if (key === 'userName') {
+            if(IsValidateName(value)) {
+                setUserName(value);
+            } else {
+                setUserName('잘못된 입력값');
+            }
+        }
+        if (key === 'studentNo') {
+            if(IsValidateNum(value)) {
+                setUserNumber(value);
+            } else {
+                setUserNumber('잘못된 입력값');
+            }
+        }
+        if (key === 'email') {
+            if(IsValidateEmail(value)) {
+                setUserEmail(value);
+            } else {
+                setUserEmail('잘못된 입력값');
+            }
+        }
     }
 
     inputModalElement.close();
